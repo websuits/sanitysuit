90566aac6fe8643c2310f939c6366a71dfc277d6
Stefan Luntraru <stefanluntraru@Stefans-MacBook-Air-2.local>
2024-07-10 01:13:44 +0300

    main: menu progress

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,11 +1,10 @@
 import { SchemaTypeDefinition } from 'sanity'
 
 import blockContent from './blockContent'
-import siteConfig from './siteConfig'
 import post from './post'
 import menu from './menu'
 
 export const schemaTypes = [post, menu, blockContent]
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [siteConfig, post, menu, blockContent],
+  types: [post, menu, blockContent],
 }
